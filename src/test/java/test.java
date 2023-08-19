e5585bf62f66545337017dde5979a85b5a6b0548
shortcut2yp <32234622+shortcut2yp@users.noreply.github.com>
2023-08-19 18:25:45 +0530

    updated options to use chrome 115

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -9,7 +9,7 @@ public class test {
     public void startBrowser(){
         ChromeOptions options = new ChromeOptions();
         options.setBrowserVersion("115");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.amazon.com");
         driver.quit();
     }
