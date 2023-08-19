b2821a8969c76a6735e70b3d01731eb4f8fcde08
Yash <yash.patel@smallcase.com>
2023-08-19 17:50:40 +0530

    added testng xml file

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,9 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 public class test {
-    public static void main(String[] args) {
+
+    @Test
+    public void startBrowser(){
         ChromeOptions options = new ChromeOptions();
         options.setBrowserVersion("116");
         WebDriver driver = new ChromeDriver();
